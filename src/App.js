31c2a5bb1c6d635fbafd8d56f5a666d6ebef3f0d
Samuel Rowan <samuel@samuelrowan.com>
2021-06-23 11:41:30 -0500

    added product list components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { Route, Link } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ArtPrompts from "./pages/prompts/prompts.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/prompts" component={ArtPrompts} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
