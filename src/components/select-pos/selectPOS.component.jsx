92ebed964edec81a832ae1809512e0d9f51ca63a
Samuel Rowan <samuel@samuelrowan.com>
2021-07-13 11:13:03 -0500

    changed the selectpos back

--- src/components/select-pos/selectPOS.component.jsx
+++ src/components/select-pos/selectPOS.component.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import PROMPT_DATA from '../../pages/prompts/prompts.data';
 
 const selectPOS = (pos) => {
-    return pos;
+    
+    return (
+        PROMPT_DATA[pos][Math.floor(Math.random()*PROMPT_DATA.pos.length)]
+    )
 }
 
 export default selectPOS
