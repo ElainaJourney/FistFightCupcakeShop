9310d40983cc871980734c6ed37b265d96c1b98b
Samuel Rowan <samuel@samuelrowan.com>
2021-07-22 17:41:34 -0500

    finished shopping bag

--- src/components/bag-components/bag-dropdown/bag-dropdown.component.jsx
+++ src/components/bag-components/bag-dropdown/bag-dropdown.component.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import CustomButton from '../../custom-button/custom-button.component'
+import BagItem from '../bag-item/bag-item.component'
+
 import './bag-dropdown.styles.scss'
 
-const BagDropdown = () => (
+const BagDropdown = ({ bagItems }) => (
     <div className='bag-dropdown'>
-        <div className='bag-items'/>
+        <div className='bag-items'>
+            {bagItems.map(bagItem => <BagItem key={bagItem.id} item={bagItem}/>)}
+        </div>
         <CustomButton>Checkout</CustomButton>
     </div>
 )
 
-export default BagDropdown
+const mapStateToProps = ({ bag: { bagItems } }) => ({
+    bagItems
+})
+
+export default connect(mapStateToProps)(BagDropdown)
